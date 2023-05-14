f2c56d950d0c146a03537cc56d2690e9047797d3
akalaminamin <alamin@hypescout.co>
2023-05-14 18:24:42 +0600

    get the signigicant services

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express =require('express')
 const app =express()
 const port =process.env.PORT || 5000;
 const cors =require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 //midleware
 app.use(cors())
@@ -25,7 +25,23 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-
+const serviceCollection = client.db('carDoctor').collection('services')
+// get the all services 
+app.get('/services', async(req,res)=>{
+  const cursor = serviceCollection.find()
+  const result = await cursor.toArray();
+  res.send(result)
+})
+//get special services 
+app.get('/services/:id',async(req,res)=>{
+  const id = req.params.id;
+  const  query ={_id : new ObjectId(id)}
+  const options = {
+    projection : {title:1,service_id:1,price:1}
+  }
+  const result =await serviceCollection.findOne(query,options)
+  res.send(result)
+})
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
