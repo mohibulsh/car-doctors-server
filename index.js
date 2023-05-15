d1659f1477d98d36fc938cbb24dc5e9cd8d5555c
akalaminamin <alamin@hypescout.co>
2023-05-15 19:04:13 +0600

    add the booking

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ const client = new MongoClient(uri, {
   }
 });
 const serviceCollection = client.db('carDoctor').collection('services')
+const bookingCollection = client.db('carDoctor').collection('booking')
 // get the all services 
 app.get('/services', async(req,res)=>{
   const cursor = serviceCollection.find()
@@ -37,11 +38,29 @@ app.get('/services/:id',async(req,res)=>{
   const id = req.params.id;
   const  query ={_id : new ObjectId(id)}
   const options = {
-    projection : {title:1,service_id:1,price:1}
+    projection : {title:1,service_id:1,price:1,img:1}
   }
   const result =await serviceCollection.findOne(query,options)
   res.send(result)
 })
+//some data get depends on email
+app.get('/bookings',async(req,res)=>{
+  // console.log(req.query)
+  let query ={}
+  if(req.query?.email){
+    query ={email: req.query.email}
+  }
+  const cursor = bookingCollection.find()
+  const result = await cursor.toArray()
+  res.send(result)
+})
+//isnert the data get the client sites to the database
+app.post('/bookings',async(req,res)=>{
+  const booking =req.body;
+  const result =await bookingCollection.insertOne(booking)
+  res.send(result)
+})
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
